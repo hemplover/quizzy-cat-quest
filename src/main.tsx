49a7c180ccd01d3596f3e2e17647ac5a70d9126e
hemplover <hoeslinils@gmail.com>
2025-04-07 15:03:34 +0200

    fix(auth): Wrap App with AuthProvider in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,24 @@
 // Importazione PDF.js prima di tutto per configurare il worker
 import './utils/pdfjs-init';
 
+import React from 'react';
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 import './index.css'
+import { AuthProvider } from './contexts/AuthContext';
+import { Toaster } from './components/ui/sonner';
 
 // Make sure we mount the React app with the BrowserRouter properly
 const root = createRoot(document.getElementById("root")!);
 
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+        <Toaster />
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
